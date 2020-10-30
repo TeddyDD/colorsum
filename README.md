1f923e922aeb0921d2dd07ecb00e89087fdf9d5f
Daniel Lewan <vision360.daniel@gmail.com>
2020-10-30 19:42:43 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# colorsum
+
+Inspired by this task on Rosetta Code:
+
+https://rosettacode.org/wiki/Checksumcolor
+
+## Usage
+
+```sh
+sha256sum files | colorsum.lua
+```
---
+++ colorsum.lua
@@ -0,0 +1,44 @@
+#!/usr/bin/env lua
+
+--[[
+    inspired by
+    https://rosettacode.org/wiki/Checksumcolor
+]]
+
+function lerp(a, b, v)
+    return a + (b - a) * v
+end
+
+local cprint = (function()
+    local c = {}
+    for i=31,37 do table.insert(c,i) end
+    local fmt = '\27[%sm%s\27[0m'
+    return function(line)
+        local ok, _,sum = line:find("(.+)%s.+")
+        if not ok then print(line) return end
+        local idx = 1
+        for n in sum:gmatch("%x%x%x") do
+            num = tonumber("0x"..n)
+            io.write(fmt:format(
+                c[math.floor(lerp(1,#c,num/4096) + 0.5)],
+                n
+            ))
+            idx = idx + 3
+        end
+        io.write(string.format("%s\n", line:sub(idx)))
+    end
+end)()
+
+local function isTTY(fd)
+    fd = tonumber(fd) or 1
+    local ok, exit, signal = os.execute(string.format("test -t %d", fd))
+    return (ok and exit == "exit") and signal == 0 or false
+end
+
+for line in io.stdin:lines() do
+    if not isTTY() then
+        print(line)
+    else
+        cprint(line)
+    end
+end
